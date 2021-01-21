17d02cbd42a283909a2aec6eaa2b830932e97a0c
slbor <sl.borisov.42@outlook.com>
2021-01-22 01:11:39 +0400

    Some edits

--- main.cpp
+++ main.cpp
@@ -6,17 +6,23 @@
 #include <map>
 #include <set>
 #include <omp.h>
+#include <sstream>
 
 using namespace std;
 
 vector<vector<int>> generateGraph(long long numOfVertices, long long numOfEdges);
 vector<vector<int>> generateGraph(long long numOfVertices);
-void generateDot(const vector<vector<int>> &graph, const string& filename);
+void generateDot(const vector<vector<int>> &graph, const string &filename);
+void generateColorDot(const vector<vector<int>> &graph, const vector<vector<int>> &subgraph, const string &filename);
 vector<vector<vector<int>>> getSubgraphs(const vector<vector<int>> &graph, long long int numOfEdges);
 vector<vector<int>> getSubgraph(const vector<vector<int>> &graph, long long first, long long numOfEdges);
+bool searchSubgraph(vector<vector<vector<int>>> allSubgraphs, const vector<vector<int>> &neededSubgraphs);
+bool compareSubgraph(vector<vector<int>> subgraph1, vector<vector<int>> subgraph2);
 vector<vector<int>> gs(const vector<vector<int>> &graph, long long first, long long numOfEdges);
 vector<vector<int>> validSubgraph(const vector<vector<int>> &graphs);
 vector<vector<int>> normalize(vector<int> &g);
+
+bool iAmDolban = true;//help me!
 
 int main() {
     double tm = omp_get_wtime();
@@ -25,11 +31,32 @@ int main() {
     generateDot(graph, "graph.dot");
     vector<vector<int>> subgraph = getSubgraph(graph, 1, 6);
     generateDot(subgraph, "subgraph.dot");
-    cout << (double)((long long)((omp_get_wtime() - tm) * 10)) / 10 << " s" << endl;
+    //cout << (double) ((long long) ((omp_get_wtime() - tm) * 10)) / 10 << " s" << endl;
 
     tm = omp_get_wtime();
-    vector<vector<vector<int>>> subgraphs = getSubgraphs(graph, 8);
-    cout << (double)((long long)((omp_get_wtime() - tm) * 10)) / 10 << " s" << endl;
+    vector<vector<vector<int>>> subgraphs = getSubgraphs(graph, 6);
+
+    if (iAmDolban) {
+        for (int i = 0; i < subgraphs.size(); i++) {
+            string s = "allSubgraphs/Subgraph ", numb;
+            stringstream ss;
+            ss << (i);
+            ss >> numb;
+            s += numb;
+            s += ".dot";
+            generateDot(subgraphs[i], s);
+        }
+
+        generateColorDot(graph, subgraph, "colorGraph.dot");
+    }
+
+    if (searchSubgraph(subgraphs, subgraph)) {
+        cout << "Subgraph was found\n";
+    } else {
+        cout << "Subgraph not found\n";
+    }
+
+    //cout << (double) ((long long) ((omp_get_wtime() - tm) * 10)) / 10 << " s" << endl;
     return 0;
 }
 
@@ -69,7 +96,7 @@ vector<vector<int>> generateGraph(long long numOfVertices) {
     return generateGraph(numOfVertices, numOfEdges);
 }
 
-void generateDot(const vector<vector<int>> &graph, const string& filename) {
+void generateDot(const vector<vector<int>> &graph, const string &filename) {
     ofstream fileOut(filename);
     fileOut << "graph {\n";
     long long len = graph.size();
@@ -86,7 +113,7 @@ void generateDot(const vector<vector<int>> &graph, const string& filename) {
         if (str[str.size() - 2] == '-') {
             for (int j = 0; j < 4; ++j)
                 str.pop_back();
-            fileOut << str << ";\n";
+            //fileOut << str << ";\n";
         } else {
             str[str.size() - 2] = ';';
             fileOut << str << endl;
@@ -110,7 +137,7 @@ vector<vector<vector<int>>> getSubgraphs(const vector<vector<int>> &graph, long 
     if (out.empty()) {
         return static_cast<vector<vector<vector<int>>>>(0);
     } else {
-        vector<vector<vector<int>>> v (out.begin(), out.end());
+        vector<vector<vector<int>>> v(out.begin(), out.end());
         return v;
     }
 }
@@ -131,30 +158,49 @@ vector<vector<int>> getSubgraph(const vector<vector<int>> &graph, long long int 
 }
 
 vector<vector<int>> normalize(vector<int> &g) {
-    map<int, int> m;
     vector<vector<int>> subgraph;
+    if (!iAmDolban) {
+        map<int, int> m;
 
-    for (int &i : g) {
-        if (m.count(i) == 0) {
-            int sz = m.size();
-            m.insert(pair<int, int>(i, sz));
+
+        for (int &i : g) {
+            if (m.count(i) == 0) {
+                int sz = m.size();
+                m.insert(pair<int, int>(i, sz));
+            }
         }
-    }
 
-    vector<int> ng(m.size());
+        vector<int> ng(m.size());
 
-    subgraph.reserve(m.size());
-    for (int i = 0; i < m.size(); ++i) {
-        subgraph.push_back(ng);
-    }
+        subgraph.reserve(m.size());
+        for (int i = 0; i < m.size(); ++i) {
+            subgraph.push_back(ng);
+        }
 
-    for (int &i : g) {
-        i = m[i];
-    }
+        for (int &i : g) {
+            i = m[i];
+        }
 
-    for (int i = 0; i < g.size() - 1; i++) {
-        subgraph[g[i]][g[i + 1]] = 1;
-        subgraph[g[i + 1]][g[i]] = 1;
+        for (int i = 0; i < g.size() - 1; i++) {
+            subgraph[g[i]][g[i + 1]] = 1;
+            subgraph[g[i + 1]][g[i]] = 1;
+        }
+
+    } else {//  I am so sorry
+        int _max = 0;
+        for (int &i : g)
+            _max = max(_max, i);
+
+        vector<int> ng(_max + 1);
+        subgraph.reserve(_max + 1);
+        for (int i = 0; i < _max + 1; ++i) {
+            subgraph.push_back(ng);
+        }
+        for (int i = 0; i < g.size() - 1; i++) {
+            subgraph[g[i]][g[i + 1]] = 1;
+            subgraph[g[i + 1]][g[i]] = 1;
+        }
+        int y = 9;
     }
     return subgraph;
 }
@@ -177,7 +223,7 @@ vector<vector<int>> gs(const vector<vector<int>> &graph, long long int first, lo
         bufSubgraphs = subgraphs;
         subgraphs.clear();
 
-        for (auto & bufSubgraph : bufSubgraphs) {
+        for (auto &bufSubgraph : bufSubgraphs) {
             vector<int> buf;
             buf.push_back(first);
             buf.insert(buf.end(), bufSubgraph.begin(), bufSubgraph.end());
@@ -204,4 +250,70 @@ vector<vector<int>> validSubgraph(const vector<vector<int>> &graphs) {
     }
 
     return validatedGraphs;
+}
+
+bool searchSubgraph(vector<vector<vector<int>>> allSubgraphs, const vector<vector<int>> &neededSubgraphs) {
+    bool check = false;
+
+    for (int i = 0; i < allSubgraphs.size(); i++)
+        if (compareSubgraph(allSubgraphs[i], neededSubgraphs)) {
+            check = true;
+        }
+
+    return check;
+}
+
+bool compareSubgraph(vector<vector<int>> subgraph1, vector<vector<int>> subgraph2) {
+    bool check = true;
+    vector<int> buf(max(subgraph1.size(), subgraph2.size()), 0);
+    subgraph1.resize(max(subgraph1.size(), subgraph2.size()), buf);
+    subgraph2.resize(max(subgraph1.size(), subgraph2.size()), buf);
+    for (int i = 0; i < max(subgraph1.size(), subgraph2.size()); i++) {
+        subgraph1[i].resize(max(subgraph1[i].size(), subgraph2[i].size()), 0);
+        subgraph2[i].resize(max(subgraph1[i].size(), subgraph2[i].size()), 0);
+
+        for (int ii = 0; ii < max(subgraph1[i].size(), subgraph2[i].size()); ii++) {
+            if (subgraph1[i][ii] == 1) {
+                if (subgraph2[i][ii] != 1) {
+                    check = false;
+                    return check;
+                }
+            }
+        }
+    }
+    return check;
+}
+
+void generateColorDot(const vector<vector<int>> &graph, const vector<vector<int>> &subgraph, const string &filename) {
+    ofstream fileOut(filename);
+    fileOut << "graph {\n";
+    long long len = graph.size();
+    long long subLen = subgraph.size();
+
+    for (long long i = 0; i < subLen; ++i) {
+        bool f = false;
+        for (long long ii = 0; ii < subLen; ++ii) {
+            if (subgraph[i][ii]) {
+                f = true;
+            }
+        }
+        if (f) {
+            fileOut << "    " + to_string(i) + " [color=red];" << endl;
+        }
+    }
+
+    for (long long i = 0; i < len; ++i) {
+        for (long long ii = i; ii < len; ++ii) {
+            if (graph[i][ii]) {
+                fileOut << "    " + to_string(i) + " -- " + to_string(ii);
+                if (subLen > i && subLen > ii && subgraph[i][ii]) {
+                    fileOut << " [color=red];" << endl;
+                } else {
+                    fileOut << ";" << endl;
+                }
+            }
+        }
+    }
+
+    fileOut << '}';
 }
